b5818c3087812655ae45641ad01fbce8bb915aa2
Allison Schulman (she/her) <68669789+sneakin-allie@users.noreply.github.com>
2022-02-02 15:15:15 -0500

    Quick Hits section done

--- src/components/QuickHits.js
+++ src/components/QuickHits.js
@@ -1,38 +1,39 @@
-import { StyledQuickHits } from "./styles/QuickHits.styled";
+import { StyledList, StyledPara, StyledQuickHits } from "./styles/QuickHits.styled";
 
 export default function QuickHits() {
     return (
         <StyledQuickHits>
-            <h2>The start of the (real-life) season loomed large, requiring a partnership that carefully balanced features, budget, and speed.</h2>
-            <ol>
-                <h3>Our Quick Hits</h3> 
-                <li>
-                    <ul>
-                        <span>Product Discovery</span>
-                        <li>Wireframes</li>
-                        <li>UX Design</li>
-                    </ul>
-                </li>
-                <li>
-                    <ul>
-                        <span>Experience Design</span>
-                        <li>Branding</li>
-                        <li>Design System</li>
-                        <li>UI Design</li>
-                    </ul>
-                </li>
-                <li>
-                    <ul>
-                    <span>Development</span> 
-                    <li>Elixir + Phoenix</li>
-                    <li>GraphQL</li>
-                    <li>React</li>
-                    </ul>
-                </li>
-            </ol>
-            <svg width="1168" height="2" viewBox="0 0 1168 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <line y1="1" x2="1168" y2="1" stroke="#BDC8C8" stroke-width="2"/>
-            </svg>
+            <StyledPara>
+                <h2>The start of the (real-life) season loomed large, requiring a partnership that carefully balanced features, budget, and speed.</h2>
+            </StyledPara>
+            <StyledList>
+                <h3>Our Quick Hits</h3>
+                <ol> 
+                    <li>
+                        <ul>
+                            <li>Product Discovery</li>
+                            <li>Wireframes</li>
+                            <li>UX Design</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul>
+                            <li>Experience Design</li>
+                            <li>Branding</li>
+                            <li>Design System</li>
+                            <li>UI Design</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul>
+                            <li>Development</li> 
+                            <li>Elixir + Phoenix</li>
+                            <li>GraphQL</li>
+                            <li>React</li>
+                        </ul>
+                    </li>
+                </ol>
+            </StyledList>
         </StyledQuickHits>
     )
 }
--- src/components/styles/QuickHits.styled.js
+++ src/components/styles/QuickHits.styled.js
@@ -5,55 +5,64 @@ export const StyledQuickHits = styled.section`
     grid-template-columns: repeat(16, 1fr);
     grid-column-gap: 1.5rem;
     margin-bottom: 13.5rem;
+`             
 
-    h2 { // The start of the...
-        grid-column-start: 3;
-        grid-column-end: 11;
+export const StyledPara = styled.div`
+    grid-column-start: 1;
+    grid-column-end: 11;
+
+    h2 {
         color: ${({ theme }) => theme.colors.darkgreen};
         margin: 0;
+        border-bottom: 2px solid ${({ theme }) => theme.colors.silver};
+        padding: 0 0 13.5rem 19.5rem;
+        line-height: 4.25rem;
     }
+`
 
-    svg {
-        grid-column-start: 1;
-        grid-column-end: 11;
-        margin-top: -13.5rem;
-    }
+export const StyledList = styled.div`
+    grid-column-start: 12;
+    grid-column-end: 16;
 
-    h3 { // Our Quick Hits
+    h3 {
         color: ${({ theme }) => theme.colors.darkgreen};
-        margin: 0 0 3rem 0;
+        margin: 0 0 2.5rem 0;
     }
 
     ol {
         font-family: "Space Grotesk";
         font-weight: normal;
-        grid-column-start: 12;
-        grid-column-end: 16;
         padding: 0;
         margin: 0;
-    }
-
-    span { // Product Discovery, Experience Design, Development
-        font-family: "IBM Plex Serif";
-        font-weight: bold;
-        font-size: 1.875em;
-        color: ${({ theme }) => theme.colors.gunmetalgray};
-        line-height: 2em;
-        margin: 0;
+        list-style-type: none;
+        counter-reset: count;
     }
 
     ul {
         list-style-type: none;
-    }
-
-    ul > li { // rest of list items
         font-family: "IBM Plex Serif";
-        font-weight: normal;
-        font-size: 1.5em;
         color: ${({ theme }) => theme.colors.gunmetalgray};
-        line-height: 2em;
-        padding: none;
-        margin: 0;
-    }
+        line-height: 2.5rem;
+        padding: 0 0 3rem 0;
 
-`             
+        li:first-child::before {
+            counter-increment: count;
+            content: counter(count, decimal-leading-zero);
+            font-family: "Space Grotesk";
+            color: ${({ theme }) => theme.colors.darkgreen};
+            padding-right: 4.5rem;
+
+        }
+
+        li:first-child {
+            font-weight: bold;
+            font-size: 1.875em;
+        }
+
+        li:nth-child(1n + 2) {
+            font-weight: normal;
+            font-size: 1.5em;
+            padding-left: 6.8rem;
+        }
+    }
+`
