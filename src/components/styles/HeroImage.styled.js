b69021efbbb0a3b85f1d14c52ed55b8cb46504ed
Allison Schulman (she/her) <68669789+sneakin-allie@users.noreply.github.com>
2022-02-08 14:09:57 -0500

    Premise is responsive (moving from top to bottom)

--- src/components/styles/HeroImage.styled.js
+++ src/components/styles/HeroImage.styled.js
@@ -4,4 +4,8 @@ export const StyledHeroImage = styled.div`
     display: flex;
     justify-content: center;
     margin: -18.75rem 12.625rem 9rem 12.625rem;
+
+    @media (max-width: 1040px) {
+        margin: -24.75rem 6.625rem 3rem 6.625rem;
+    }
 `
--- src/components/styles/Premise.styled.js
+++ src/components/styles/Premise.styled.js
@@ -5,6 +5,18 @@ export const StyledPremise = styled.section`
     display: grid;
     grid-template-columns: repeat(16, 1fr);
     grid-column-gap: 1.5rem;
+
+    @media (max-width: 1040px) {
+        margin-top: 4.5rem;
+    }
+
+    @media (max-width: 800px) {
+        margin-top: 13.5rem;
+    }
+
+    @media (max-width: 560px) {
+        margin-top: 18rem;
+    }
 `
 
 export const StyledTitle = styled.div`
@@ -13,11 +25,19 @@ export const StyledTitle = styled.div`
     font-size: 5.625em;
     color: ${({ theme }) => theme.colors.darkgreen};
     grid-column-start: 4;
-    grid-column-end: 9;
+    grid-column-end: 13;
     margin-bottom: 1.125rem;
 
     p {
         margin: 0;
+
+        @media (max-width: 1040px) {
+            font-size: 4rem;
+        }
+
+        @media (max-width: 800px) {
+            font-size: 3rem;
+        }
     }
 `
 
@@ -29,4 +49,12 @@ export const StyledBody = styled.div`
     line-height: 1.6em;
     grid-column-start: 4;
     grid-column-end: 13;
+
+    @media (max-width: 1280px) {
+        grid-column-end: 14;
+    }
+
+    @media (max-width: 1040px) {
+        grid-column-end: 15;
+    }
 `
