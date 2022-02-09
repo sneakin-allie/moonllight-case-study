dbe35209defcbb93188b00f5025bb765d5c4f1c2
Allison Schulman (she/her) <68669789+sneakin-allie@users.noreply.github.com>
2022-02-09 13:54:01 -0500

    Footer is responsive

--- src/components/styles/Footer.styled.js
+++ src/components/styles/Footer.styled.js
@@ -9,7 +9,21 @@ export const FooterGrid = styled.div`
     display: grid;
     grid-template-columns: repeat(16, 1fr);
     grid-column-gap: 1.5rem;
-    margin: 0 4.5rem 4.5rem 4.5rem;
+    margin: 0 4.5rem 0 4.5rem;
+
+    @media (max-width: 1350px) {
+        grid-column-gap: 1rem;
+        margin: 0 2.5rem 0rem 2.5rem;
+    }
+
+    @media (max-width: 1280px) {
+        grid-column-gap: 0rem;
+        margin: 0 1rem 0rem 1rem;
+    }
+
+    @media (max-width: 1040px) {
+        margin: 0 0 0rem 0;
+    }
 
     p {
         padding: 0;
@@ -34,12 +48,23 @@ export const FooterGrid = styled.div`
     }
 `
 
+// --- PRACTICAL APPLICATIONS --- //
+
 export const StyledPractApp = styled.div`
     grid-column-start: 2;
-    grid-column-end: 10;
+    grid-column-end: 8;
 
     @media (max-width: 1040px) {
-        
+        grid-column-start: 2;
+        grid-column-end: 16;
+    }
+
+    @media (max-width: 800px) {
+        grid-column-start: 4;
+    }
+
+    @media (max-width: 560px) {
+        grid-column-start: 3;
     }
 
     p {
@@ -49,8 +74,22 @@ export const StyledPractApp = styled.div`
         font-size: 9.375em;
         line-height: 9.375rem;  
 
+        @media (max-width: 1600px) {
+            font-size: 7em;
+        }
+
+        @media (max-width: 1280px) {
+            font-size: 6em;
+            line-height: 8rem;
+        }
+
         @media (max-width: 1040px) {
-            font-size: 5em;
+            font-size: 4.5em;
+        }
+
+        @media (max-width: 800px) {
+            line-height: 6rem;
+            margin-bottom: 4rem;
         }
     }
 
@@ -64,6 +103,21 @@ export const StyledNavigate = styled.ul`
     grid-column-end: 13;
     margin: 0;
     padding: 0;
+
+    @media (max-width: 1040px) {
+        grid-column-start: 5;
+        grid-column-end: 8;
+    }
+
+    @media (max-width: 800px) {
+        grid-column-start: 5;
+        grid-column-end: 9;
+    }
+
+    @media (max-width: 560px) {
+        grid-column-start: 4;
+        grid-column-end: 8;
+    }
 `
 
 export const StyledExplore = styled.ul`
@@ -73,17 +127,41 @@ export const StyledExplore = styled.ul`
     margin: 0;
     padding: 0;
 
+    @media (max-width: 1040px) {
+        grid-column-start: 10;
+        grid-column-end: 13;
+    }
+
+    @media (max-width: 800px) {
+        grid-column-start: 10;
+        grid-column-end: 14;
+    }
+
+    @media (max-width: 560px) {
+        grid-column-start: 10;
+        grid-column-end: 15;
+    }
+
     svg {
         padding-right: 1rem;
+
+        @media (max-width: 1280px) {
+            padding-right: 0.5rem;
+        }
     }
 `
 
-export const StyledNav = styled.nav`
+export const NavGrid = styled.nav`
     background-color: ${({ theme }) => theme.colors.darkestgreen};
     display: grid;
     grid-template-columns: repeat(16, 1fr);
     grid-column-gap: 1.5rem;
     padding: 2.5rem 0;
+    margin: 0;
+
+    @media (max-width: 1600px) {
+        grid-column-gap: 0rem;
+    }
 `
 export const StyledLogoFooter = styled.svg`
     grid-column-start: 2;
@@ -91,25 +169,21 @@ export const StyledLogoFooter = styled.svg`
 `
 
 export const StyledContact = styled.div`
-    grid-column-start: 9;
-    grid-column-end: 16;
+    grid-column-start: 10;
+    grid-column-end: 17;
     padding-top: 1rem;
 
-    @media (max-width: 1500px) {
+    @media (max-width: 1600px) {
         grid-column-start: 8;
     }
 
-    @media (max-width: 1330px) {
-        grid-column-start: 7;
-    }
-
-    @media (max-width: 1210px) {
+    @media (max-width: 1280px) {
         grid-column-start: 6;
     }
 
-    @media (max-width: 1110px) {
+    @media (max-width: 1040px) {
         grid-column-start: 2;
-        padding-top: 1.5rem;
+        padding-top: 2.5rem;
     }
 
     h6 { // GET IN TOUCH
@@ -118,6 +192,11 @@ export const StyledContact = styled.div`
         letter-spacing: 0.25em;
         margin: 0;
         vertical-align: middle;
+
+        @media (max-width: 800px) {
+            display: block;
+            margin-bottom: 0.5rem;
+        }
     }
     
     p { // email & phone
@@ -128,6 +207,18 @@ export const StyledContact = styled.div`
         color: ${({ theme }) => theme.colors.palemint};
         margin: 0;
         vertical-align: middle;
+
+        @media (max-width: 800px) {
+            border-right: 4px solid ${({ theme }) => theme.colors.gunmetalgray};
+            padding-right: 1.5rem;
+            margin-right: 1.5rem;
+        }
+
+        @media (max-width: 560px) {
+            border-right: 4px solid ${({ theme }) => theme.colors.gunmetalgray};
+            padding-right: 0.5rem;
+            margin-right: 0.5rem;
+        }
     }
 
     svg { // icons for email & phone
@@ -137,47 +228,38 @@ export const StyledContact = styled.div`
         padding-left: 1.5rem;
         margin-right: 0.5rem;
         vertical-align: middle;
+
+        @media (max-width: 800px) {
+            border-left: none;
+            margin-left: 0;
+            padding-left: 0;
+            margin-right: 0.5rem;
+        }
     }
 `
 
 // --- Motifs --- //
 
 export const StyledTealSquare = styled.div`
-    background-color: ${({ theme }) => theme.colors.teal};
-    grid-column-start: 9;
-    height: 3rem;
-    opacity: 0.5;
-    width: 3rem;
+    grid-column-start: 10;
+    
+    div {
+        background-color: ${({ theme }) => theme.colors.teal};
+        height: 3rem;
+        opacity: 0.5;
+        width: 3rem;
+    }
 `
 
 export const StyledOmbreSquares = styled.div`
     grid-column-start: 8;
-    position: relative;
-
-    svg {
-        position: absolute;
-        top: 4.5rem;
-    }
+    grid-column-end: 10;
 `
 
 export const StyledX = styled.div`
     grid-column-start: 1;
-    position: relative;
-
-    svg {
-        position: absolute;
-        right: 2rem;
-        top: 10rem;
-    }
 `
 
 export const StyledSquareGroup = styled.div`
     grid-column-start: 1;
-    position: relative;
-
-    svg {
-        position: absolute;
-        bottom: 0;
-    }
-
 `
